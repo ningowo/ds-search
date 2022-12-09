f3560f5dd79634d7829bc2c1a23bba038ad96b26
Siying Chen <chensiyingcrystal@gmail.com>
2022-12-09 10:39:08 -0800

    Update VoteRequestOrBuilder.java

--- src/main/java/team/dsys/dssearch/cluster/raft/impl/message/VoteRequestOrBuilder.java
+++ src/main/java/team/dsys/dssearch/cluster/raft/impl/message/VoteRequestOrBuilder.java
@@ -6,7 +6,6 @@ import io.microraft.RaftEndpoint;
 import io.microraft.model.message.VoteRequest;
 import io.microraft.model.message.VoteRequest.VoteRequestBuilder;
 import team.dsys.dssearch.cluster.raft.RaftNodeEndpoint;
-
 import javax.annotation.Nonnull;
 
 public class VoteRequestOrBuilder implements VoteRequest, VoteRequestBuilder, RaftMessageRequestAware {
